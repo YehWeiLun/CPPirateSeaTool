989755e552c34ed338ef7781cfcd6c70a36dc3a2
YehWeiLun <wly52394@gmail.com>
2024-03-09 18:27:24 +0800

    test

---
+++ Layout/script.js
@@ -0,0 +1,4 @@
+var link = document.createElement('link');
+link.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+link.rel="stylesheet";
+document.head.appendChild(link);
