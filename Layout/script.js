885f43f7a6e527759985b91b24e5307c22aebd1a
YehWeiLun <wly52394@gmail.com>
2024-03-16 23:39:45 +0800

    更新layout ```

--- Layout/script.js
+++ Layout/script.js
@@ -1,4 +1,23 @@
-(function(){
-    console.log('loaded');
+(function(){        
+    class PageModifier{        
+        constructor(){
+            this.forms = getAllActionsForm();
+            this.actions = [this.forms.map(f=>f.querySelector("input[name=mode]").value)];
+            console.log(this.actions);
+        }
+    }
+    function getAllActionsForm(){
+        var inputs = document.querySelectorAll('input[name=mode]');
+        var forms = [];
+        inputs.forEach(ipt=>{
+            let currentParentNode = ipt.parentNode;
+            while(currentParentNode && currentParentNode.nodeName!='FORM'){                
+                currentParentNode = currentParentNode.parentNode;
+                if(currentParentNode.nodeName =='FORM') forms.push(currentParentNode);
+            }            
+        });
+        return forms;
+    }
 
+    var pageModifier = new PageModifier();
 })()
