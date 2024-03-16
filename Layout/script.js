3d0eb208a4d4bc04b27506e41d31625e602aa115
YehWeiLun <wly52394@gmail.com>
2024-03-17 02:45:29 +0800

    解鎖右鍵+取得所有表單 ```

--- Layout/script.js
+++ Layout/script.js
@@ -1,23 +1,47 @@
-(function(){        
-    class PageModifier{        
-        constructor(){
-            this.forms = getAllActionsForm();
-            this.actions = [this.forms.map(f=>f.querySelector("input[name=mode]").value)];
-            console.log(this.actions);
+(function () {
+    class ActionData {
+        constructor(form) {
+            this.name = form.querySelector('input[name=mode]').value;
+            this.form = form;
         }
     }
-    function getAllActionsForm(){
+    class PageModifier {
+        constructor() {
+            // 取得當前頁面的所有表單
+            this.actions = getAllActionsForm().map(f => new ActionData(f));
+            // 解除右鍵限制
+            unlockRightClick();
+            console.log.apply(null, this.actions.map(a => a.name));
+        }
+    }
+    /// 取得所有表單
+    function getAllActionsForm() {
         var inputs = document.querySelectorAll('input[name=mode]');
         var forms = [];
-        inputs.forEach(ipt=>{
+        inputs.forEach(ipt => {
             let currentParentNode = ipt.parentNode;
-            while(currentParentNode && currentParentNode.nodeName!='FORM'){                
+            if (currentParentNode.nodeName == 'FORM') forms.push(currentParentNode);
+            while (currentParentNode && currentParentNode.nodeName != 'FORM') {
                 currentParentNode = currentParentNode.parentNode;
-                if(currentParentNode.nodeName =='FORM') forms.push(currentParentNode);
-            }            
+                if (currentParentNode.nodeName == 'FORM') forms.push(currentParentNode);
+            }
         });
         return forms;
     }
+    /// 解除右鍵限制
+    function unlockRightClick() {
+        function R(a) {
+            ona = "on" + a;
+            if (window.addEventListener) window.addEventListener(a, function (e) { for (var n = e.originalTarget; n; n = n.parentNode) n[ona] = null; }, true);
+            window[ona] = null; document[ona] = null; if (document.body) document.body[ona] = null;
+        }
+        R("contextmenu");
+        R("click");
+        R("mousedown");
+        R("mouseup");
+        R("selectstart");        
+    }
 
     var pageModifier = new PageModifier();
+   
 })()
